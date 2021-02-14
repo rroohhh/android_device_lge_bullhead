lakgarg/bullhead-11

bullhead: Make shim for AudioSystem::setErrorCallback()
 * Commit 000c3e4 in fw/av changed setErrorCallback() to
   addErrorCallback() to support multiple instances, but
   this breaks our old blob.

Change-Id: I03495e5746a4ad75a09a0e24bb7faf265a8b0bda

BoardConfig.mk
@@ -168,6 +168,10 @@ TARGET_FS_CONFIG_GEN += device/lge/bullhead/config.fs
 
 -include vendor/lge/bullhead/BoardConfigVendor.mk
 
+# Shims
+TARGET_LD_SHIM_LIBS := \
+    /system/vendor/lib64/libril-qc-qmi-1.so|libaudioclient_shim.so
+
 # Testing related defines
 BOARD_PERFSETUP_SCRIPT := platform_testing/scripts/perf-setup/bullhead-setup.sh
 

rroohhh/android_device_lge_bullhead

BoardConfig.mk
@@ -161,6 +161,10 @@ TARGET_USES_OLD_MNC_FORMAT := true
 BOARD_SEPOLICY_DIRS += \
     device/lge/bullhead/sepolicy
 
+# Shims
+TARGET_LD_SHIM_LIBS := \
+    /system/vendor/lib/libril-qc-qmi-1.so|libaudioclient_shim.so
+
 # WiFi
 BOARD_HAS_QCOM_WLAN := true
 BOARD_WLAN_DEVICE := qcwcn